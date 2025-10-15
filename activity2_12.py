44097184e4e48fd066ba5518d49b30bef5fd9f38
SahejTheWarrior <sahejburande@gmail.com>
2025-10-15 19:11:08 +0100

    Add files via upload

---
+++ activity2_12.py
@@ -0,0 +1,9 @@
+#fluids triangle
+row=int(input("please enter the numbeer of rows: "))
+num=1
+print("floyds triangle")
+for i in range(1, row+1):
+    for j in range(1, i+1):
+        print(num,end = " ")
+        num=num+1
+    print()
